2fb02776c1caf46019be046aab5de424cfbf728d
Luke Taylor <luke@circleleasing.co.uk>
2023-05-24 10:10:20 +0100

    Scaffold server

---
+++ app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const path = require('path');
+const fs = require('fs');
+const os = require('os');
+
+const express = require('express');
+const formData = require('express-form-data');
+
+function loadRoutes(server, routeDir){
+	const routes = fs.readdirSync(routeDir).filter(file => file.endsWith('.js'));
+
+	routes.forEach(r => {
+		const route = require(path.join(routeDir, r));
+		server.use(route.root, route.router);
+	});
+}
+
+const server = new express();
+server.use(express.json());
+server.use(express.urlencoded({
+	extended: true
+}));
+
+loadRoutes(server, "./routes");
+
+server.use(formData.parse({
+	uploadDir: os.tmpdir(),
+	autoclean: true
+}));
+
+server.use(formData.format());
+
+server.listen(80, () => {
+	console.log("Listening on port 80!");
+});
